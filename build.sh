b514951142d991acaedc6eb15eb73bc189d561f5
Sam Thomas <sgpthomas@gmail.com>
2017-04-27 11:59:27 -0400

    added comment to build

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 echo "Building Shrdlu..."
+
+# getting sources from build.list
 IFS=$'\n' read -d '' -r -a lines < build.list
 echo "Using Sources:"
 printf ' * %s\n' "${lines[@]}"
+
+# make build folder, copy src into it, and run build
 mkdir build && cd build
 cp -r ../src .
 ocamlc -o shrdlu ${lines[@]}
+
+# move executable to main directory, clean build
 mv shrdlu ..
 cd ..
 rm -r build
+
 echo "Done"
