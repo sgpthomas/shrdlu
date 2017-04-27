441d4b08e60db1ca6aa88d2f5358ca827a7c66a0
Sam Thomas <sgpthomas@gmail.com>
2017-04-27 11:57:52 -0400

    updated the build script to read file names from build.list

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 echo "Building Shrdlu..."
+IFS=$'\n' read -d '' -r -a lines < build.list
+echo "Using Sources:"
+printf ' * %s\n' "${lines[@]}"
 mkdir build && cd build
 cp -r ../src .
-ocamlc -o shrdlu src/main.ml
+ocamlc -o shrdlu ${lines[@]}
 mv shrdlu ..
 cd ..
 rm -r build
