bb271ea5265254a110560705c4e94c303798f81f
Hyuckin David Lim <hyuckin.lim15@gmail.com>
2017-05-01 13:22:04 -0400

    added inline explicit type signatures for functions in parser.ml

--- src/parser.ml
+++ src/parser.ml
@@ -6,8 +6,8 @@ let string_of_leaf = function
   | Leaf(str) -> str
   | _ -> raise Tree_not_found
 
-let rec extract_info model instruction_tree address =
-    let entity = at instruction_tree address in
+let rec extract_info (m : model) (instruction : tree) (address : int list) =
+    let entity = at instruction address in
     let color = if ((at entity [0;1]) = (Leaf "0")) then (Leaf "colorless")
                 else (at entity [0;1;0]) in
     let color = string_of_leaf color in
@@ -19,61 +19,61 @@ let rec extract_info model instruction_tree address =
                     then (at entity [0;adj_address;0;1])
                     else (at entity [0;adj_address;0;0]) in
     let direction = string_of_leaf direction in
-    let (c, s, al) = extract_info model entity [0;adj_address;1] in
-    let adjacent_entity = find_ID model (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
+    let (c, s, al) = extract_info m entity [0;adj_address;1] in
+    let adjacent_entity = find_ID m (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
     [(direction,adjacent_entity)] in
-    (color,shape,adjacent);;
+    (color,shape,adjacent)
 
-let create_command model instruction =
+let create_command (m : model) (instruction : string) =
     let tree_list = (wrapper command instruction) in
     if List.length tree_list <> 0 then
       let tree1 = List.hd tree_list in
-      let (c, s, al) = extract_info model tree1 [0;1] in
+      let (c, s, al) = extract_info m tree1 [0;1] in
       Create (Entity (!(genid ()), (shape_of_string s), (color_of_string c)), (List.map adjacent_of_string al))
     else
       raise Tree_not_found
 
-let delete_command model instruction =
+let delete_command (m : model) (instruction : string) =
     let tree_list = (wrapper command instruction) in
     if List.length tree_list <> 0 then
       let tree1 = List.hd tree_list in
-      let (c, s, al) = extract_info model tree1 [0;1] in
-      let id = find_ID model (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
+      let (c, s, al) = extract_info m tree1 [0;1] in
+      let id = find_ID m (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
       Delete (id)
     else
       raise Tree_not_found
 
-let paint_command model instruction =
+let paint_command (m : model) (instruction : string) =
     let tree_list = (wrapper command instruction) in
     if List.length tree_list <> 0 then
       let tree1 = List.hd tree_list in
-      let (c, s, al) = extract_info model tree1 [0;1] in
-      let id = find_ID model (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
+      let (c, s, al) = extract_info m tree1 [0;1] in
+      let id = find_ID m (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
       let new_color = string_of_leaf (at tree1 [0;2;0]) in
       Paint (id, color_of_string new_color)
     else
       raise Tree_not_found
 
-(* let move_command mode instruction =
+(* let move_command (m : model) instruction =
     let tree_list = (wrapper command instruction) in
     if List.length tree_list <> 0 then
       let tree1 = List.hd tree_list in
-      let (c, s, al) = extract_info model tree1 [0;1] in
-      let id = find_ID model (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
+      let (c, s, al) = extract_info m tree1 [0;1] in
+      let id = find_ID m (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
       let new_color = at tree1 [0;2;1;0] in
       Paint (id, color)
     else
       raise Tree_not_found *)
 
-let parse model instruction =
+let parse (m : model) (instruction : string) =
   if instruction = "" then Error ("type something") else
     let first_word = List.hd (String.split_on_char ' ' instruction) in
     try
       match first_word with
-      | "create" -> create_command model instruction
-      | "delete" -> delete_command model instruction
-      | "paint" -> paint_command model instruction
-      (* | "move" -> move_command model instruction *)
+      | "create" -> create_command m instruction
+      | "delete" -> delete_command m instruction
+      | "paint" -> paint_command m instruction
+      (* | "move" -> move_command m instruction *)
       | "#print" -> Print
       | "#reset" -> Reset
       | _ -> Error ("Unable to grasp meaning")
