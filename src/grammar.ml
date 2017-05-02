be91edd680babd60c9bed15b338add4c7b25e112
Hyuckin David Lim <hyuckin.lim15@gmail.com>
2017-05-02 18:57:28 -0400

    removed redundant code in parser. changed grammar to separate number from quantifier

--- src/grammar.ml
+++ src/grammar.ml
@@ -27,8 +27,8 @@ let opt p = p |. empty
 let (noanswers : ('a * remainder) list) = []
 
 let (any_one_of : string list -> tree combinatorparser) l = function
-    x::xs when (List.mem x l) -> [((Leaf x),xs)]
-    | _ -> noanswers
+  x::xs when (List.mem x l) -> [((Leaf x),xs)]
+  | _ -> noanswers
 
 (* The custom grammar itself *)
 let rec command words = (query |. create |. delete |. paint |. move >. unary "Command") words
@@ -42,35 +42,36 @@ and entity words = ( opt(d) &. (thatLessentity |. thatFulentity) >. binary "Enti
 and thatLessentity words = (( opt(color) &. shape &. opt(adjacent)) >. ternary "thatLessEntity") words
 and thatFulentity words = (( opt(color) &. shape &. (terminal "that") &. adjacent) >. fourary "thatFulEntity") words
 and adjacent words = (direction &. entity >. binary "Adjacent") words
-and quantifier = (any_one_of ["least";"exactly";"most";"more";"less"] &. any_one_of ["1";"2";"3";"4";"5";"6";"7";"8";"9";"10"]) >. binary "Quantifier"
+and quantifier words = ((any_one_of ["least";"exactly";"most";"more";"less"] &. number ) >. binary "Quantifier") words
+and number = (any_one_of ["0";"1";"2";"3";"4";"5";"6";"7";"8";"9";"10"]) >. unary "Number"
 and d = (any_one_of ["the";"a";"an"]) >. unary "D"
 and shape = (any_one_of ["cube";"sphere";"pyramid"]) >. unary "Shape"
 and color = (any_one_of ["red";"orange";"yellow";"white";"green";"blue";"purple"]) >. unary "Color"
 and direction = ((any_one_of ["above";"below";"behind";"front"]) >. unary "Direction")
-      |. ((opt(terminal "the") &. (terminal "left" |. terminal "right")) >. binary "Direction")
+    |. ((opt(terminal "the") &. (terminal "left" |. terminal "right")) >. binary "Direction")
 
 (* Wrapper that parses the sentence *)
 let wrapper (p : tree combinatorparser) words =
   let remove_garbage words =
-  	let remove_list = ["to";"in";"of";"is";"are";"than";"at"] in
-    let predicate x = not (List.mem x remove_list) in List.filter predicate words
+  let remove_list = ["to";"in";"of";"is";"are";"than";"at"] in
+  let predicate x = not (List.mem x remove_list) in List.filter predicate words
   in
   let swap_list = [("any","least 1");("cubes","cube");("spheres","sphere");("pyramids","pyramid");("one","1");
-  				("two","2");("three","3");("four","4");("five","5");("six","6");("seven","7");("eight","8");
-  				("nine","9");("ten","10")] in
+        ("two","2");("three","3");("four","4");("five","5");("six","6");("seven","7");("eight","8");
+        ("nine","9");("ten","10")] in
   let swap words =
-  	let helper word =
-     if (List.mem_assoc word swap_list) then
-  	   ((String.split_on_char ' ' (List.assoc word swap_list)))
-     else [word]
+  let helper word =
+   if (List.mem_assoc word swap_list) then
+     ((String.split_on_char ' ' (List.assoc word swap_list)))
+   else [word]
    in
    List.map helper words in
   let finished analysis = match analysis with
-    | (_,[]) -> true
-    | _ -> false
+  | (_,[]) -> true
+  | _ -> false
   in
   List.map Pervasives.fst (List.filter finished
-                             (p (remove_garbage (List.flatten (swap (String.split_on_char ' ' words))))  ))
+               (p (remove_garbage (List.flatten (swap (String.split_on_char ' ' words))))  ))
 
 (* Address finder *)
 exception Tree_not_found
@@ -78,31 +79,31 @@ let rec at myTree address =
   match address with
   | [] -> myTree
   | head :: tail -> match myTree with
-    | Leaf(_) -> if [head] = [] then myTree else raise Tree_not_found
-    | Branch(value, tree_list) -> if head >= List.length tree_list then raise Tree_not_found else
-        at (List.nth tree_list head) tail
+  | Leaf(_) -> if [head] = [] then myTree else raise Tree_not_found
+  | Branch(value, tree_list) -> if head >= List.length tree_list then raise Tree_not_found else
+    at (List.nth tree_list head) tail
 
 let dot_of_tree title t =
   let rec dot_of_node i = function
-      Leaf name ->  (("n"^(string_of_int i)^" [label = \""^name^"\"];\n"),i)
-    | Branch (parent,kids) ->
-        let (rootbyindexlist,maximum) = List.fold_left (fun (sofar,index) kid ->
-          let (result,newindex) = dot_of_node (index+1) kid in
-          ((result,(index+1))::sofar,newindex)
-                                        )
-            ([],i)
-            kids in
-        let thisnode = ("n"^(string_of_int i)^" [label = \""^parent^"\"];\n") in
-        let downarrows = List.fold_left (fun already (subtree,index) ->
-          ("n"^(string_of_int i)^"-> n"^(string_of_int index)^";\n"^already)
-                        )
-            ""
-            rootbyindexlist in
-        let subtreedot = List.fold_left (fun already (subtree,index) ->
-                                             subtree^already)
-            ""
-            rootbyindexlist in
-        (thisnode^downarrows^subtreedot,maximum)
+    Leaf name ->  (("n"^(string_of_int i)^" [label = \""^name^"\"];\n"),i)
+  | Branch (parent,kids) ->
+    let (rootbyindexlist,maximum) = List.fold_left (fun (sofar,index) kid ->
+      let (result,newindex) = dot_of_node (index+1) kid in
+      ((result,(index+1))::sofar,newindex)
+                    )
+      ([],i)
+      kids in
+    let thisnode = ("n"^(string_of_int i)^" [label = \""^parent^"\"];\n") in
+    let downarrows = List.fold_left (fun already (subtree,index) ->
+      ("n"^(string_of_int i)^"-> n"^(string_of_int index)^";\n"^already)
+            )
+      ""
+      rootbyindexlist in
+    let subtreedot = List.fold_left (fun already (subtree,index) ->
+                       subtree^already)
+      ""
+      rootbyindexlist in
+    (thisnode^downarrows^subtreedot,maximum)
 
  in
   ("digraph \""^title^"\" {\n graph [bgcolor = \"transparent\"]; \n node [shape = plaintext fontcolor = white]; \n edge [arrowhead = none color = white]; \n"^(Pervasives.fst (dot_of_node 0 t))^"}")
--- src/parser.ml
+++ src/parser.ml
@@ -6,129 +6,141 @@ let string_of_leaf = function
   | Leaf(str) -> str
   | _ -> raise Tree_not_found
 
+(* The most important function in parser. Given the address of an entity
+ * a tree, it will extract its color, shape, and adjacent entities. Works
+ * recursively by also extracting the info of the adjacent entities.
+ *)
 let rec extract_info (m : model) (instruction : tree) (address : int list) =
-    let entity = at instruction address in
-    let color =
-      if ((at entity [1;0]) = (Leaf "0")) then
-        if (at instruction [0] = Leaf "0") then (Leaf "unknown") else
-        if (at instruction [0;0] = Leaf "create") then
-          (Leaf "white")
-        else
-          (Leaf "unknown")
-      else
-        (at entity [1;0;0]) in
-    let color = string_of_leaf color in
-    let shape = string_of_leaf (at entity [1;1;0]) in
-    let temp_direction = at entity [1;2] in
-    let adjacent = if temp_direction = Leaf("0") then [] else
-    let adj_address = if (temp_direction = Leaf("that")) then 3 else 2 in
-    let direction = if ((at entity [1;adj_address;0;0]) = (Leaf "the") || (at entity [1;adj_address;0;0]) = (Leaf "0"))
-                    then (at entity [1;adj_address;0;1])
-                    else (at entity [1;adj_address;0;0]) in
-    let direction = string_of_leaf direction in
+  let entity = at instruction address in
+  let color = string_of_leaf (
+    if ((at entity [1;0]) = (Leaf "0")) then
+      (* If color not defined for create command, default to white *)
+      if (at instruction [0;0] = Leaf "create") then (Leaf "white")
+      (* If color not defined for other commands, then color unknown *)
+      else (Leaf "unknown")
+    (* Color has been explicitly defined *)
+    else (at entity [1;0;0])
+  ) in
+  let shape = string_of_leaf (at entity [1;1;0]) in
+  let maybe_direction = at entity [1;2] in
+  let adjacent = 
+    (* No adjacent entities *)
+    if (maybe_direction = Leaf("0")) then [] 
+    else let adj_address = if (maybe_direction = Leaf("that")) then 3 else 2 in
+    let direction = string_of_leaf (
+      if ((at entity [1;adj_address;0;0]) = (Leaf "the") || (at entity [1;adj_address;0;0]) = (Leaf "0"))
+      then (at entity [1;adj_address;0;1])
+      else (at entity [1;adj_address;0;0]) 
+    ) in
     let (c, s, al) = extract_info m entity [1;adj_address;1] in
-    let adjacent_entity = find_ID m (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
-    [(direction,adjacent_entity)] in
-    (color,shape,adjacent)
+    let adjacent_entity = find_ID m c s al in
+  [(direction,adjacent_entity)] in
+  (color_of_string color, shape_of_string shape, (List.map adjacent_of_string adjacent))
 
 let bracket_tree (tr : tree) =
   let rec get_all_leaves (sl : string list) (t : tree) =
-    match t with
-    | Leaf (s) -> List.append sl [s]
-    | Branch (s, tree_list) -> List.flatten (List.map (get_all_leaves sl) tree_list)
+  match t with
+  | Leaf (s) -> List.append sl [s]
+  | Branch (s, tree_list) -> List.flatten (List.map (get_all_leaves sl) tree_list)
   in
   let rec string_of_list = function
-    | [] -> ""
-    | hd::tl -> hd ^ " " ^ (string_of_list tl)
+  | [] -> ""
+  | hd::tl -> hd ^ " " ^ (string_of_list tl)
   in
   match at tr [0] with
   | Leaf (s) -> Printf.sprintf "( %s )" s
   | Branch (s, tree_list) ->
-    String.filter (fun x -> x <> '0') (string_of_list ( (List.map (fun l -> "( " ^ string_of_list l ^ ")") (List.map (fun t -> (get_all_leaves []) t) tree_list))))
+    String.filter (fun x -> x <> '0') 
+    (string_of_list 
+      (List.map (fun l -> "( " ^ string_of_list l ^ ")") 
+        (List.map (fun t -> (get_all_leaves []) t) tree_list)
+      )
+    )
 
 let resolve_ambiguity (tl : tree list) =
   let sz = List.length tl in
   if sz = 1 then
-    List.hd tl
+  List.hd tl
   else
-    begin
-      List.iteri (fun i t -> Printf.printf " [%d] -> %s\n" i (bracket_tree t)) tl ;
-      let rec p () =
-        Printf.printf "enter 0-%d to choose:\n -> " (sz-1) ;
-        try
-          let choice = read_int () in List.nth tl (choice)
-        with
-        | _ -> p ()
-      in
-      p ()
-    end
+  begin
+    List.iteri (fun i t -> Printf.printf " [%d] -> %s\n" i (bracket_tree t)) tl ;
+    let rec p () =
+    Printf.printf "enter 0-%d to choose:\n -> " (sz-1) ;
+    try
+      let choice = read_int () in List.nth tl (choice)
+    with
+    | _ -> p ()
+    in
+    p ()
+  end
 
 let get_tree (instruction : string) = 
-    let tree_list = (wrapper command instruction) in
-    if List.length tree_list <> 0 then
-    resolve_ambiguity tree_list
-    else raise Tree_not_found
+  let tree_list = (wrapper command instruction) in
+  if List.length tree_list <> 0 then
+  resolve_ambiguity tree_list
+  else raise Tree_not_found
 
 let create_command (m : model) (instruction : string) =
-    let tree = get_tree instruction in
-      let (c, s, al) = extract_info m tree [0;1] in
-      Create (Entity (!(genid ()), (shape_of_string s), (color_of_string c)), (List.map adjacent_of_string al))
+  let tree = get_tree instruction in
+    let (c, s, al) = extract_info m tree [0;1] in
+    Create (Entity (!(genid ()), s, c), al)
 
 let delete_command (m : model) (instruction : string) =
-    let tree = get_tree instruction in
-      let (c, s, al) = extract_info m tree [0;1] in
-      let id = find_ID m (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
-      Delete (id)
+  let tree = get_tree instruction in
+    let (c, s, al) = extract_info m tree [0;1] in
+    let id = find_ID m c s al in
+    Delete (id)
 
 let paint_command (m : model) (instruction : string) =
-    let tree = get_tree instruction in
-      let (c, s, al) = extract_info m tree [0;1] in
-      let id = find_ID m (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
-      let new_color = string_of_leaf (at tree [0;2;0]) in
-      Paint (id, color_of_string new_color)
+  let tree = get_tree instruction in
+    let (c, s, al) = extract_info m tree [0;1] in
+    let id = find_ID m c s al in
+    let new_color = string_of_leaf (at tree [0;2;0]) in
+    Paint (id, color_of_string new_color)
 
 let move_command (m : model) (instruction : string) =
-    let tree = get_tree instruction in
-      let (c, s, al) = extract_info m tree [0;1] in
-      let id = find_ID m (color_of_string c) (shape_of_string s) (List.map adjacent_of_string al) in
-      let (c2, s2, al2) = extract_info m tree [0;3] in
-      let id2 = find_ID m (color_of_string c2) (shape_of_string s2) (List.map adjacent_of_string al2) in
-      let direction =
-        if ((at tree [0;2;0]) = (Leaf "the") || (at tree [0;2;0]) = (Leaf "0"))
-        then (at tree [0;2;1])
-        else (at tree [0;2;0]) in
-      let direction = string_of_leaf direction in
-      Move (id, [Adjacent ((direction_of_string direction), id2)])
+  let tree = get_tree instruction in
+    let (c, s, al) = extract_info m tree [0;1] in
+    let id = find_ID m c s al in
+    let (c2, s2, al2) = extract_info m tree [0;3] in
+    let id2 = find_ID m c2 s2 al2 in
+    let direction = string_of_leaf (
+      if ((at tree [0;2;0]) = (Leaf "the") || (at tree [0;2;0]) = (Leaf "0"))
+      then (at tree [0;2;1])
+      else (at tree [0;2;0]) 
+    ) in
+    Move (id, [Adjacent (direction_of_string direction, id2)])
 
 let exists_command (m : model) (instruction : string) =
-    let tree = get_tree instruction in
-      let quantifier = quantifier_of_string (string_of_leaf (at tree [0;0;1;0]))
-      (int_of_string(string_of_leaf (at tree [0;0;1;1]))) in
-      let (c, s, al) = extract_info m tree [0;0;2] in
-      Exist (quantifier, ((color_of_string c),(shape_of_string s),(List.map adjacent_of_string al)))
+  let tree = get_tree instruction in
+    let quantifier = quantifier_of_string (string_of_leaf (at tree [0;0;1;0]))
+    (int_of_string (string_of_leaf (at tree [0;0;1;1;0]))) in
+    let (c, s, al) = extract_info m tree [0;0;2] in
+    Exist (quantifier, (c, s,al))
 
 let parse (m : model) (instruction : string) =
   if instruction = "" then Error ("type something") else
-    let first_word = List.hd (String.split_on_char ' ' instruction) in
-    try
-      match first_word with
-      | "create" -> create_command m instruction
-      | "delete" -> delete_command m instruction
-      | "paint" -> paint_command m instruction
-      | "move" -> move_command m instruction
-      | "are"
-      | "is" -> exists_command m instruction
-      | "#exists" -> Exist (Exactly (3), (Red, Cube, []))
-      | "#move" -> Move (3, [Adjacent (Right, 1)])
-      | "#print" -> Print
-      | "#reset" -> Reset
-      | "#parse" -> List.iteri (writetree) (wrapper command (snd (String.split instruction " "))) ; Error ("parsed tree")
-      | _ -> Error ("Unable to grasp meaning")
-    with
-    | No_such_adjacent_exception -> Error ("No such adjacent")
-    | No_such_direction_exception -> Error ("No such direction")
-    | No_such_shape_exception -> Error ("No such shape")
-    | No_such_color_exception -> Error ("No such color")
-    | No_such_entity_exception (msg) -> Error (Printf.sprintf "Unable to find this '%s' in the model" msg)
-    | Tree_not_found -> Error ("Failed to parse input")
-    | _ -> Error ("Something unexpected went wrong")
+  let first_word = List.hd (String.split_on_char ' ' instruction) in
+  try
+    match first_word with
+    | "create" -> create_command m instruction
+    | "delete" -> delete_command m instruction
+    | "paint" -> paint_command m instruction
+    | "move" -> move_command m instruction
+    | "are"
+    | "is" 
+    | "there" -> exists_command m instruction
+    | "#print" -> Print
+    | "#reset" -> Reset
+    | "#parse" -> List.iteri (writetree) (wrapper command (snd (String.split instruction " "))) ; Error ("parsed tree")
+    | _ -> Error ("Unable to grasp meaning")
+  with
+  | No_such_adjacent_exception -> Error ("No such adjacent")
+  | No_such_direction_exception -> Error ("No such direction")
+  | No_such_shape_exception -> Error ("No such shape")
+  | No_such_color_exception -> Error ("No such color")
+  | No_such_entity_exception (msg) -> Error (Printf.sprintf "Unable to find this '%s' in the model" msg)
+  | Tree_not_found -> Error ("Failed to parse input")
+  | _ -> Error ("Something unexpected went wrong")
+
+
